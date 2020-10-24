fa7e6454f9d20b957840907a275cc562a4f5f229
Kendricks <codelyok13@live.com>
2020-10-23 23:14:40 -0400

    First

---
+++ autoBuildStartQemu.ps1
@@ -0,0 +1,8 @@
+﻿#Place this file in teh location of your iso
+
+#Have Linux update code and iso
+wsl ./run.sh
+
+$iso = "myos.iso" #name of iso
+$qemu = ".\qemu-w64-setup-20200201\qemu-system-x86_64.exe" #path of qemu executable
+Invoke-Expression "$qemu -cdrom $iso"
